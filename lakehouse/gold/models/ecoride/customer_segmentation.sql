5eb2a655316b58b228adbde212b7fb68354ca21f
ThaliaBarrera <thalia.barrera.dorantes@gmail.com>
2024-04-09 21:06:40 +0200

    Update gold models

--- lakehouse/gold/models/ecoride/customer_segmentation.sql
+++ lakehouse/gold/models/ecoride/customer_segmentation.sql
@@ -9,7 +9,7 @@ SELECT
     c.country,
     COUNT(s.id) as total_purchases,
     AVG(s.sale_price) as average_purchase_value,
-    LIST_AGG(DISTINCT v.model_name, ', ') as preferred_models
+    LISTAGG(DISTINCT v.model_name, ', ') as preferred_models
 FROM {{ source('silver', 'customers') }} AT branch {{ nessie_branch }} c
 LEFT JOIN {{ source('silver', 'sales') }} AT branch {{ nessie_branch }} s ON c.id = s.customer_id
 LEFT JOIN {{ source('silver', 'vehicles') }}  AT branch {{ nessie_branch }} v ON s.vehicle_id = v.id
--- lakehouse/gold/models/vehicle_health/vehicle_health_analysis.sql
+++ lakehouse/gold/models/vehicle_health/vehicle_health_analysis.sql
@@ -1,10 +1,9 @@
 {% set nessie_branch = var('nessie_branch', 'main') %}
 
 SELECT
-    vh.VehicleID,
-    vh.ManufacturingYear,
-    vh.Model,
-    COUNT(JSON_LENGTH(vh.Alerts)) as total_alerts,
-    JSON_EXTRACT_SCALAR(MAX(vh.Alerts, '$.Date')) as latest_alert_date
-FROM {{ source('silver', 'vehicle_health_logs') }} AT branch {{ nessie_branch }} vh
-GROUP BY vh.VehicleID, vh.ManufacturingYear, vh.Model
+    VehicleID,
+    Model,
+    ManufacturingYear,
+    Alerts,
+    MaintenanceHistory
+FROM {{ source('silver', 'vehicle_health_logs') }} AT branch {{ nessie_branch }}
